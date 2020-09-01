e74183db61ec694a9b2bd0ad304ea3815b6ec389
JohnLi002 <44849474+JohnLi002@users.noreply.github.com>
2020-09-01 15:43:15 -0400

    started to mess with the icons and titles

--- beginning.py
+++ beginning.py
@@ -5,3 +5,20 @@ pygame.init()
 
 #Create the screen
 screen = pygame.display.set_mode((800,600))
+
+running = True
+
+
+#Title and Icon
+pygame.display.set_caption("This is a game")
+icon = pygame.image.load("Pentagon.png")
+pygame.display.set_icon(icon)
+
+
+#Allows screen to continue existing until it is exited
+while running:
+    #Checks for events that happen to the screen
+    for event in pygame.event.get():
+        #if the exit button on the screen is pressed the loops will stop
+        if event.type == pygame.QUIT:
+            running = False
